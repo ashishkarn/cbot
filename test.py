da6f662406c57b79d13a4e31161444aac4d1c800
Ashish Karn <ashish.karan96@gmail.com>
2020-07-03 18:51:23 +0530

    treat you better

---
+++ test.py
@@ -0,0 +1,44 @@
+# bot.py
+import os
+
+TOKEN="NzI4NTYwMjExOTY2ODIwNDIz.Xv8QWg.MceE8_DuGXqm95CstX55tOkPHnw"
+import discord
+from datetime import datetime
+import time
+
+client = discord.Client()
+typeSwitch=False
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('saurabh sir padha do'):
+        channel=message.channel
+        print(message.content)
+        await channel.send('link lelo beta. www.udemy.com')
+
+    elif message.content.startswith('Beta dikkat ho jayegi'):
+        channel=message.channel
+        print("test")
+        await channel.send('Sir ki Aagya maniye aap log thik h naa.')
+
+@client.event
+async def on_typing(channel, user, when):
+    global typeSwitch
+    global lapsed_time
+    if(typeSwitch is False):
+        epoch_time = (when - datetime(1970, 1, 1)).total_seconds()
+        lapsed_time=epoch_time+15
+        typeSwitch=True
+        
+    print(lapsed_time)
+    print(time.time())
+    
+    if(time.time()>lapsed_time):
+        await channel.send('Hamari class me mobile chala rahe hain aap @{0}'.format(str(user)))
+        typeSwitch=True
+
+client.run(TOKEN)
